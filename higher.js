50e76ad9e35a9ebb3098b0bda11d2f3e6aebe98f
Hina Kawauchi <hina_17n1100142@nnn.ed.jp>
2017-07-10 16:00:51 +0900

    高階関数の利用

--- higher.js
+++ higher.js
@@ -1,12 +1,16 @@
-function arryWalk(data,f){
-    for(var key in data){
-        f(data[key],key);
+function arryWalk(data, f) {
+    for (var key in data) {
+        f(data[key], key);
     }
 }
 
-function showElement(value,key){
+function showElement(value, key) {
     console.log(key + ":" + value);
 }
 
-var ary = [1,2,4,8,16];
-arryWalk(ary,showElement);
+function showDecorattedElement(value, key) {
+    console.log('キー(' + key + ') : 値(' + value + ')');
+}
+
+var ary = [1, 2, 4, 8, 16];
+arryWalk(ary, showElement);
