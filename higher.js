40c7d6488d066db020f32d6453da3312e2526c3d
Hina Kawauchi <hina_17n1100142@nnn.ed.jp>
2017-07-10 15:02:05 +0900

    引数をわたすように変更

--- higher.js
+++ higher.js
@@ -6,12 +6,13 @@ function myHigherFunction(func) {
 }
 
 //普通の関数
-function outsideFunction(){
-    console.log("outsideFunction")
+function outsideFunction(value){
+    console.log("value :" + value);
 }
 
-var returnValue = myHigherFunction();
-returnValue();
+// var returnValue = myHigherFunction();
+// returnValue("引数だよ");
+myHigherFunction()("引数だよ！")
 
 //outsideFunction(); //←普通の関数よびだし
 //myHigherFunction(outsideFunction);//関数渡し
